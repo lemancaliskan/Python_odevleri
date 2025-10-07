3512b02c474a9f145c3054d1cd5e5f338bcb0c66
Leman Caliskan <jmleman95@gmail.com>
2025-10-07 22:27:48 +0300

    Odev1

---
+++ Odev1.py
@@ -0,0 +1,19 @@
+﻿    """
+    ❓ DEĞERLENDİRME SORULARI
+    "10" + 5 işlemi neden hata verir? Nasıl düzeltilir?
+
+            //"10"-->string 5-->integer. Farklı veri tipleri olduğu için doğrudan toplanamaz.//
+
+    0.1 + 0.2 == 0.3 ifadesi neden False döner?
+
+            //0.1+0.2=0.3 matematikte decimal sistem kullandığımız için doğrudur. Ancak bilgisayar binary kullandığı için cevap False döner.
+            0.3 sonucunu elde etmek istiyorsak; round(0.1 + 0.2, 1) şeklinde sayıyı yuvarlamalıyız.//
+
+    and ve or operatörlerinin öncelik sırası nedir?
+            //1)and  2)or//
+
+
+    input() fonksiyonu neden her zaman string döndürür?
+            //Kullanıcının klayveden girdiği veri string olarak alınır. Sayısal veri isteniyorsa gerekli veri tipine dönüştürülür.//
+
+    """
